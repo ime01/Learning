5d6a32ca4fbfb1a32df943f385caa09a4dd5a2a3
imevbore <ketuimevbore@yahoo.com>
2023-01-08 15:58:58 +0100

    codility Odd Occurences Solution

--- src/main/kotlin/OddOccurencesInArray.kt
+++ src/main/kotlin/OddOccurencesInArray.kt
@@ -7,7 +7,7 @@ fun main(args: Array<String>){
 
     val list = listOf(9, 3, 9, 3, 9, 7, 9)
 
-    oddOccurrencesInArray(list)
+    oddOccurrencesInArray2(list)
 
 }
 
@@ -29,4 +29,28 @@ fun oddOccurrencesInArray(numbers:List<Int>): Int{
 
     println(unpairedElement)
     return unpairedElement
+}
+
+fun oddOccurrencesInArray2(numbers:List<Int>): Int{
+
+
+    val hashSet = HashSet<Int>()
+
+
+    for ( index in numbers.indices){
+
+        val value = numbers[index]
+
+        if (hashSet.contains(value)){
+            hashSet.remove(value)
+        }else{
+           hashSet.add(value)
+        }
+
+
+    }
+
+
+    println(hashSet.iterator().next())
+    return  hashSet.iterator().next()
 }
